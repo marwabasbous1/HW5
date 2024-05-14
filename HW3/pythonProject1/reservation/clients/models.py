7ebc0fd1114848de5e5174d629ef5c817e807896
Marwa Basboos <114429564+marwabasbous1@users.noreply.github.com>
2024-05-15 00:37:58 +0400

    assim8

--- HW3/pythonProject1/reservation/clients/models.py
+++ HW3/pythonProject1/reservation/clients/models.py
@@ -1,14 +1,36 @@
-from django.db import models
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .models import Client, ClientType, Product, Order
 
-from django.db import models
+class ClientTypeForm(forms.ModelForm):
+    class Meta:
+        model = ClientType
+        fields = '__all__'
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=50, choices=[
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+class ProductForm(forms.Form):
+    PRODUCT_CATEGORIES = [
         ('Food', 'Food'),
         ('Snacks', 'Snacks'),
         ('Drinks', 'Drinks'),
         ('Hardware', 'Hardware'),
-    ])
-    description = models.TextField()
-    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    ]
+
+    product_name = forms.CharField(max_length=100)
+    category = forms.ChoiceField(choices=PRODUCT_CATEGORIES)
+    description = forms.CharField(widget=forms.Textarea)
+    rating = forms.IntegerField(min_value=1, max_value=5)
