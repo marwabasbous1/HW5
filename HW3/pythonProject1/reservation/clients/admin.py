65d6742c0779f58b2c2bd47f27d0aec2465f0dc4
Marwa Basboos <114429564+marwabasbous1@users.noreply.github.com>
2024-05-21 00:57:31 +0400

    aasim8

--- HW3/pythonProject1/reservation/clients/admin.py
+++ HW3/pythonProject1/reservation/clients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Client, ClientType, Product, Order
+
+admin.site.register(ClientType)
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Order)
