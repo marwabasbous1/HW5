6f80be70e6291328d0eb5f240c139df06f2325cc
Marwa Basboos <114429564+marwabasbous1@users.noreply.github.com>
2024-05-21 00:58:58 +0400

    assim8

--- HW3/pythonProject1/reservation/clients/forms.py
+++ HW3/pythonProject1/reservation/clients/forms.py
@@ -1,9 +1,36 @@
-# forms.py
-from itertools import product
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .models import Client, ClientType, Product, Order
 
-from django.forms import forms
+class ClientTypeForm(forms.ModelForm):
+    class Meta:
+        model = ClientType
+        fields = '__all__'
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = '__all__'
 
 class ProductForm(forms.ModelForm):
     class Meta:
-        model = product
+        model = Product
         fields = '__all__'
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+class ProductForm(forms.Form):
+    PRODUCT_CATEGORIES = [
+        ('Food', 'Food'),
+        ('Snacks', 'Snacks'),
+        ('Drinks', 'Drinks'),
+        ('Hardware', 'Hardware'),
+    ]
+
+    product_name = forms.CharField(max_length=100)
+    category = forms.ChoiceField(choices=PRODUCT_CATEGORIES)
+    description = forms.CharField(widget=forms.Textarea)
+    rating = forms.IntegerField(min_value=1, max_value=5)
