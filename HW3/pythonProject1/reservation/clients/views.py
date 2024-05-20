72eb5fb812f0d163b3da24cc38739e5181f965bd
Marwa Basboos <114429564+marwabasbous1@users.noreply.github.com>
2024-05-21 00:54:21 +0400

    assim8

--- HW3/pythonProject1/reservation/clients/views.py
+++ HW3/pythonProject1/reservation/clients/views.py
@@ -24,3 +24,17 @@ def addproduct(request):
 #
     return render(request, 'product.html', {'form': form})
 
+def add_client(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm()
+
+    return render(request, 'add_client.html', {'form': form})
+
+def client_list(request):
+    clients = Client.objects.all()
+    return render(request, 'client_list.html', {'clients': clients})
